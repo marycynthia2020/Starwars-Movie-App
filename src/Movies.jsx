b88a23986b5e10a52759d013922a5064b6e11c2e
Chinemerem Ugbaja <marycynthiaconfirm@gmail.com>
2024-08-22 16:44:35 +0100

    Chinemerem commit

--- src/Movies.jsx
+++ src/Movies.jsx
@@ -1,20 +1,31 @@
 import React, { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import logo from "./assets/starwarslogo.svg";
-import { nanoid } from 'nanoid';
-import "./App.css"
+import { nanoid } from "nanoid";
+import "./App.css";
 import { darkModeContext } from "./DarkModeProvider.jsx";
+import MovieDetails from "./MovieDetails.jsx";
 
 const Movies = () => {
+  const { darkMode, toggleDarkMode } = useContext(darkModeContext);
 
-  const {darkMode, toggleDarkMode} = useContext(darkModeContext)
   const handleClick = () => {
-    toggleDarkMode()
-  }
+    toggleDarkMode();
+  };
 
   const [data, setData] = useState();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState();
+
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  const handleShowInfo =(item) => {
+    setSelectedItem(item)
+  }
+
+  const handleGoBack = () =>{
+    setSelectedItem(null)
+  }
 
   useEffect(() => {
     const fetchData = async () => {
@@ -31,48 +42,59 @@ const Movies = () => {
     };
 
     fetchData();
-  }, []);
-
-
-console.log(data)
+  }, [])
 
   return (
-    < div className= {darkMode? "white-container": "black-container"}>
-    <button onClick= {handleClick} className= {darkMode? "black-button": "white-button"}>{darkMode? "Dark Theme": "Light theme"}</button>
+    <div className={darkMode ? "white-container" : "black-container"}>
+      <button
+        onClick={handleClick}
+        className={darkMode ? "black-button" : "white-button"}
+      >
+        {darkMode ? "Dark Theme" : "Light theme"}
+      </button>
       <div className="logo-container">
-        <img src={logo} alt="starwars logo" className ="logo" />
+        <img src={logo} alt="starwars logo" className="logo" />
       </div>
-      {loading &&
-      <div>
-        <p>please wait, your data is loading ....</p>
-        <div className = "loading-div">
-          <img src={logo} alt= "starwars logo"/>
+      {loading && (
+        <div>
+          <p>please wait, your data is loading ....</p>
+          <div className="loading-div">
+            <img src={logo} alt="starwars logo" />
+          </div>
         </div>
-        </div>
-      }
-      {error &&
+      )}
+      {error && (
         <div>{`There is an error fetching your Data: error is ${error}`}</div>
-      }
-      <div className="movie-grid">
-      {data && 
-        data.map((items) => {
-            return (
-                <div key = {nanoid()} className="movie">
-                    <a href ="#" className="movie-title">{items.title} </a>
-                    <p className="date">{new Date(items.release_date).toLocaleDateString("en-US", {
-                    month: "long",
-                    day: "numeric",
-                    year: "numeric",
-                  })}
+      )}
+      {selectedItem? (
+        <MovieDetails selectedItem={selectedItem} handleGoBack={handleGoBack} />
+      ) : (
+        <div className="movie-grid">
+          {data &&
+            data.map((items) => {
+              return (
+                <div key={nanoid()} className="movie">
+                  <a href="#" className="movie-title">
+                    {items.title}{" "}
+                  </a>
+                  <p className="date">
+                    {new Date(items.release_date).toLocaleDateString("en-US", {
+                      month: "long",
+                      day: "numeric",
+                      year: "numeric",
+                    })}
                   </p>
-                    <p className="opening-crawl">{items.opening_crawl.split(" ").slice(0, 33).join(" ")}...</p>
-                    <a href="#" className="more-info">More Info</a>
-
+                  <p className="opening-crawl">
+                    {items.opening_crawl.split(" ").slice(0, 33).join(" ")}...
+                  </p>
+                  <button className="more-info" onClick = {() =>handleShowInfo(items)}>
+                    More Info
+                  </button>
                 </div>
-            )
-        })
-      }
-      </div>
+              );
+            })}
+        </div>
+      )}
     </div>
   );
 };
